56b67a69eec213da4ae9f0c8f1816a4464d9eca4
Developper-Grl <0709saly@gmail.com>
2023-01-05 16:22:29 +0100

    Nav

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width-device-width, initial-scale=1.0">
+     <title>Blog</title>
+     <!--CUSTOM STYLESHEET-->
+     <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+      <nav>
+          <div class="container nav_container">
+               <a href="index.php" class="nav__logo">Blog</a>
+               <ul class="nav__items">
+                   <li><a href="blog.php">Blog</a></li>
+                   <li><a href="about.php">About</li>
+                   <li><a href="services.php">Services</li>
+                   <li><a href="contact.php">Contact</li>
+                   <li><a href="signin">SignIn</li>
+                   <li class="nav__profile">
+                       <div class="avatar">
+                            <ul>
+                                <li><a href="dashboard.php">Dashboard</a></li>
+                                <li><a href="signout.php">SingOut</a></li>
+                            </ul>
+                       </div>
+                   </li>
+
+                  
+               </ul>
+          
+          </div>
+      </nav>
+</body>
+</html>
