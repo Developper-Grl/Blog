8599169bab38b993141f01cb42881fb2ce35c317
Developper-Grl <0709saly@gmail.com>
2023-01-08 20:14:55 +0100

    Add Google Fonts

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
      <link rel="stylesheet" href="./style.css">
      <!--ICONSCOUT CDN-->
      <link rel ="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+      <!--Google Fonts-->
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 </head>
 <body>
       <nav>
